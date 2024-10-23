0358bc081a7f39243739d3a12c02d72b4599273e
baylee-p <b_peterson4@mail.fhsu.edu>
2024-10-23 00:36:45 -0500

    Added tasks and js

---
+++ js/ui.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // Sidenav initialization   
+    const menus = document.querySelector(".sidenav");
+    M.Sidenav.init(menus, {edge: "right"});
+    // Add Task
+    const forms = document.querySelector(".side-form");
+    M.Sidenav.init(forms, {edge: "left"});
+});
